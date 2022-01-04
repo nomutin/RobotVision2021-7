f404208ae20fcff26a8f2b915635de51d332b486
nomutin <nomura0508@icloud.com>
2022-01-05 00:23:07 +0900

    説明とか書いた

--- robotvision2021_7/core.py
+++ robotvision2021_7/core.py
@@ -3,12 +3,14 @@
   腕立て伏せのフォームが悪かったら本気で怒号を飛ばすアプリ
 
   TODO:
-    * pose_judgementの説明書く
-    * mainの説明書く
     * 声の迫力足りない
+    * 褒める音声も入れる
+    * 『おい！！』も再生するようにする
 
 """
 
+
+import sys
 
 import cv2
 import numpy as np
@@ -34,6 +36,10 @@ def get_background():
     cap = cv2.VideoCapture(0)
     while cv2.waitKey(1) != ord("s"):
         cv2.imshow("press s key to take a background photo", cap.read()[1])
+
+        if cv2.waitKey(1) == ord("q"):
+            sys.exit()
+
     else:
         _, frame = cap.read()
         cap.release()
@@ -77,6 +83,9 @@ def stand_by(background, body):
         body.draw(src)
         cv2.imshow("Waiting until in the push-up position", src)
 
+    else:
+        sys.exit()
+
     cap.release()
     cv2.destroyAllWindows()
     body.get_max_body_height()
@@ -107,6 +116,13 @@ def create_fgmask(background, frame, kernel_size):
 def pose_judgement(body, timer, player):
     """姿勢の判定を行う関数
 
+    1. 体の高さがスタンバイ時の高さ(body.max_body_height)のLOW_FORM_RATIO倍になったとき，
+       低い姿勢になったと判断する
+    2. 低い姿勢になった時間がLOW_FORM_TIMEに満たなかった場合，怒号を飛ばす
+    3. 速度を測定する
+    4. 速度がV_THより早い場合，怒号を飛ばす
+    5. 腰の高さがしきい値より高いor低い場合，怒号を飛ばす
+
     Args:
         body(BodyCoordinates): 体の座標情報
         timer(Timer): タイマー
@@ -116,36 +132,47 @@ def pose_judgement(body, timer, player):
         None
 
     """
+
+    # 1.
     if body.max_body_height * LOW_FORM_RATIO > body.displacements[-1]:
         timer.lap_timer_start()
 
+    # 2.
     else:
         if timer.current_lap_time < LOW_FORM_TIME and timer.lap_time != 0:
             player.play('bear')
         timer.lap_timer_reset()
 
+    # 3.
     if timer.current_time > VELOCITY_MEASURE_TIME:
         if body.fps == 0:
             body.fps = len(body.displacements)
 
         v = abs(body.displacements[-1] - body.displacements[-body.fps])
+
+        # 4.
         if v > V_TH:
             player.play('fast')
 
+    # 5.
     if body.waist.y - (body.head.y + body.foot.y) / 2 > WAIST_TH:
         player.play('low')
+
     elif body.waist.y - (body.head.y + body.foot.y) / 2 < -WAIST_TH:
         player.play('high')
 
 
 def main():
-    bg = get_background()               # 背景画像を撮影
-    body = BodyCoordinates()            # 体の座標を定義
-    stand_by(background=bg, body=body)  # 腕立ての体制になるまでスタンバイ
-    timer = Timer()
-    timer.start()                       # タイマースタート
-    sound_player = SoundPlayer()        # 音楽プレーヤーを定義
+    # 背景画像を撮影
+    bg = get_background()
+    body = BodyCoordinates()
 
+    # 腕立ての体制になるまでスタンバイ
+    stand_by(background=bg, body=body)
+
+    timer = Timer()
+    timer.start()
+    sound_player = SoundPlayer()
     cap = cv2.VideoCapture(0)
 
     while cv2.waitKey(1) != ord("q"):
@@ -153,6 +180,7 @@ def main():
         fgmask = create_fgmask(background=bg, frame=frame, kernel_size=8)
         src = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
 
+        # 体の各座標を取得
         try:
             body.get_coordinates(fgmask)
 
@@ -160,16 +188,19 @@ def main():
             pass
 
         body.get_displacements()
+
+        # 姿勢を判定
         pose_judgement(body=body, timer=timer, player=sound_player)
 
+        # 図形の描画
         body.draw(image=src)
         body.draw(image=frame)
-
         cv2.imshow('mask', src)
         cv2.imshow("flow", frame)
 
-    cap.release()
-    cv2.destroyAllWindows()
+    else:
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
